e11e1b28d1dedd48747c090c911d86f9f83c8fbf
hou'si'ya <2325704386@qq.com>
2020-10-07 14:18:19 +0800

    第一次创建工程

---
+++ cmake-build-debug/CMakeFiles/github.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/github.dir/main.c.obj"
+  "github.exe"
+  "github.exe.manifest"
+  "github.pdb"
+  "libgithub.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/github.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
